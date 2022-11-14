3bb5588587bb8410e156dfa4e357c9b3cfcde344
DenisOuma <dennisouma38@gmail.com>
2022-11-14 06:25:28 +0300

    Adding the html files for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log("App - Rendered Welcome to React, This is my first React App");
+	return (
+		<div className="App">
+			<h1>React App</h1>
+			<h2>Welcome to a new Project in react</h2>
+		</div>
+	);
 }
 
 export default App;
